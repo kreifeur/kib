0666018827936dcbf12da6573ea0476793b50ae1
kreifeur <kreifeur669@gmail.com>
2024-04-15 19:34:13 +0200

    projects

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,7 @@ import ServiceCard from './ServiceCard'
 
 const Services = () => {
   return (
-    <div className=' w-full flex flex-col items-center p-5 gap-10'>
+    <div className=' w-full flex flex-col items-center p-5 gap-10 bg-gray-100'>
         <div className='flex items-center justify-between w-full sm:w-[80%] sm:flex-row flex-col gap-4'>
             <div className='sm:w-[30%] bg-gradient-to-r from-black via-gray-500 to-indigo-400 inline-block text-transparent bg-clip-text text-2xl font-bold w-full'>
                 Our service we can help you
