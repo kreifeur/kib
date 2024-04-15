34081217d991be0606513b3b484be1f8c74ebcf8
kreifeur <kreifeur669@gmail.com>
2024-04-15 15:50:04 +0200

    size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,17 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <div className="h-[90vh] w-full flex items-center justify-center flex-col gap-6">
-      <div className=" px-5 py-1 font-bold text-5xl w-[80%] bg-gradient-to-r from-black via-gray-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+      <div className=" px-5 py-1 font-bold sm:text-5xl text-2xl sm:w-[80%] w-[100%] bg-gradient-to-r from-black via-gray-500 to-indigo-400 inline-block text-transparent bg-clip-text">
         We specialize in crafting exceptional digital experiences to help our
         clients achieve their business goals .
       </div>
 
-      <div className="px-5 w-[80%] text-md text-gray-600">
+      <div className="px-5 sm:w-[80%] w-[100%] text-md text-gray-600">
         A fully integrated digital agency that will help you create beautiful
-        website and solve your problem in your company . A fully integrated
-        digital agency that will help you create beautiful website and solve
-        your problem in your company
+        website and solve your problem in your company .
       </div>
 
-      <div className=" px-5 flex gap-10 items-center w-[80%]">
+      <div className=" px-5 flex gap-10 items-center sm:w-[80%] w-[100%]">
         <Button name="Get started"></Button>
         <button>Learn more</button>
       </div>
