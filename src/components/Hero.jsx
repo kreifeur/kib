aea3e8061db67e682cfea42fc43a551809621bad
kreifeur <kreifeur669@gmail.com>
2024-04-15 15:52:03 +0200

    size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <div className="h-[90vh] w-full flex items-center justify-center flex-col gap-6">
-      <div className=" px-5 py-1 font-bold sm:text-5xl text-2xl sm:w-[80%] w-[100%] bg-gradient-to-r from-black via-gray-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+      <div className=" px-5 py-1 font-bold sm:text-5xl text-3xl sm:w-[80%] w-[100%] bg-gradient-to-r from-black via-gray-500 to-indigo-400 inline-block text-transparent bg-clip-text">
         We specialize in crafting exceptional digital experiences to help our
         clients achieve their business goals .
       </div>
