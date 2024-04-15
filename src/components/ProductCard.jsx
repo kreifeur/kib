b9b9d33b65e51b3aba89ac1c86030e6be8ba3f21
kreifeur <kreifeur669@gmail.com>
2024-04-15 19:39:44 +0200

    projects

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div className="w-full  flex flex-col gap-1 p-4  border shadow bg-white">
+    <div className="w-full  flex flex-col gap-2 p-4  border shadow bg-white">
+      <div className="text-sm font-bold">Clothes Ecommerce website</div>
+      <p className="text-gray-500 text-xs">
+        A modern ecommerce website for clothes with admin panel to add products
+      </p>
       <Image
         src="/assets/project2.png"
         width={500}
@@ -12,11 +16,7 @@ const ProductCard = () => {
         className="w-auto h-auto border "
       />
 
-      <div className="text-sm font-bold">Clothes Ecommerce website</div>
-      <p className="text-gray-500 text-xs">
-        A modern ecommerce website for clothes with admin panel to add products
-      </p>
-      <div className="text-xs flex items-center gap-4 justify-end py-2">
+      <div className="text-xs flex items-center gap-4 justify-end ">
         <button className="px-4 py-2 bg-black text-white rounded-full ">
           Live Demo
         </button>
